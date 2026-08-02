605e556d55a35e660685cb1438c69a1f6386d675
Skelley2424 <sethkelley2424@gmail.com>
2026-08-02 19:49:35 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,1 @@
+window.MJ_CONFIG={"appVersion":"2.0.0","saveKey":"marvelJourneyV2","legacyKeys":["marvelJourneyV1","marvelJourneyV2"],"categories":{"continuity":"Continuity core","doomsday":"Road to Doomsday","both":"Integrated","recommended":"Recommended"}};
